8d14c05e3fec73cc7d53fbcfb6bbcf0ad5e0fc6e
muhamuhamuha <ahmedm357@gmail.com>
2020-06-23 22:57:22 -0400

    added custombox classmethod

--- src/ExcelBox.py
+++ src/ExcelBox.py
@@ -5,10 +5,24 @@ from GUISettings import GUISettings
 gui = GUISettings()
 class ExcelBox(Tk):
 
-    def __init__(self):
+    def __init__(
+            self,
+            title: str = None,
+            color: str = None,
+            geometry: str = None,
+        ):
+        
         super().__init__()
+        self.title('ExcelBox' if title is None else title)
+        self.configure(background=gui.bg_color if color is None else color)
+        self.geometry(gui.geo if geometry is None else geometry)
+        self.mainloop()
 
-        self.title('ExcelBox')
-        self.configure(background=gui.bg_color)
-        self.geometry(gui.geo)
-        self.mainloop()    
+    @classmethod
+    def custombox(
+            cls,
+            title: str = None,
+            color: str = None,
+            geometry: str = None,
+        ):
+        return cls(title, color, geometry)
