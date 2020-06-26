c034d130e139e7a76b4746487d007d6caf36eeff
muhamuhamuha <ahmedm357@gmail.com>
2020-06-26 12:06:35 -0400

    working with icon

--- src/ExcelBox.py
+++ src/ExcelBox.py
@@ -7,6 +7,7 @@ from tkinter import (
     X,
     RIGHT,
     BOTH,
+    PhotoImage,
 )
 
 from GUISettings import GUISettings
@@ -16,16 +17,23 @@ class ExcelBox(Tk):
 
     def __init__(
             self,
-            title: str = 'Excelbox',
+            title: str = 'ExcelBox',
             color: str = gui.bg_color,
             geometry: str = gui.geo,
-            photo_path: str = str(Path.cwd() / 'images' / 'excel_icon.png')
+            photo_path: str = str(
+                Path.cwd() / 'src' / 'images' / 'excel_icon.png'
+            )
         ):
         
         super().__init__()
-        self.iconphoto(False, photo_path)
         self.title(title)
         self.configure(background=color)
+        self.tk.call(
+            'wm',
+            'iconphoto',
+            self._w,
+            PhotoImage(file=photo_path)
+        )
         self.geometry(geometry)
 
         self.mainloop()
@@ -36,27 +44,6 @@ class ExcelBox(Tk):
             title: str = None,
             color: str = None,
             geometry: str = None,
+            photo_path: str = None,
         ):
         return cls(title, color, geometry)
-
-    # @staticmethod
-    # def trialbox():
-    #     root=Tk()
-    #     def move_window(event): geometry(f'+{event.x_root}+{event.y_root}')
-
-    #     # reconfigure title bar
-    #     overrideredirect(True)
-    #     title_bar = Frame(self, bg='dark green')
-    #     close_btn = Button(title_bar, text='X', command=destroy)
-        
-
-    #     window = Canvas(self, bg=color)
-        
-    #     title_bar.pack(expand=1, fill=X)
-    #     close_btn.pack(side=RIGHT)
-    #     window.pack(expand=1, fill=BOTH)
-        
-    #     title_bar.bind('<B1-Motion>', move_window)
-
-a = ExcelBox()
-        
